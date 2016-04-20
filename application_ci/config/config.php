946c0ec907b8c1dd64ce21920def302c3fb295c6
Konstantingit <konstantin.ss@mail.ru>
2016-04-20 21:46:51 +0600

    Base url changed

--- application_ci/config/config.php
+++ application_ci/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mvc_footballblog/konstantinkz';
+$config['base_url'] = 'http://rusmatch.vvvvvv.kz';
+// $config['base_url'] = 'http://localhost/mvc_footballblog/konstantinkz';
 
 /*
 |--------------------------------------------------------------------------
