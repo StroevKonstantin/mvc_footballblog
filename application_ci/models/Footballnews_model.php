2b9b81bc601c98ac526d701b1461619448c309e9
Konstantin <rusmatch@vvvvvvkz.(none)>
2016-04-21 02:48:46 -0400

    Lives

---
+++ application_ci/models/Footballnews_model.php
@@ -0,0 +1,62 @@
+<?php
+class Footballnews_model extends CI_Model {
+
+        public function __construct(){
+                $this->load->database();
+        }
+
+        public function get_news($chpu = FALSE){
+                if ($chpu === FALSE){
+                        $this->db->order_by('id','DESC');
+                        $query = $this->db->get('news');
+                        return $query->result_array();
+                }
+
+            $query = $this->db->get_where('news', array('chpu' => $chpu));
+            return $query->row_array();
+        }
+
+        public function set_news()
+        {
+                $this->load->helper('url');
+
+                // $chpu = url_title($this->input->post('title'), 'dash', TRUE);
+
+                $data = array(
+                    'title' => $this->input->post('title'),
+                    'chpu' => $this->input->post('chpu'),
+                    'text' => $this->input->post('text')
+                );
+
+            return $this->db->insert('news', $data);
+        }
+
+        public function set_rate($chpu = FALSE)
+        {
+
+            $this->db->set('rate', 'rate+1', FALSE);
+            $this->db->where('chpu', $chpu);
+            $this->db->update('news');
+
+        }
+
+        public function get_fact($id = 1){
+
+            $query = $this->db->get_where('facts', array('id' => $id));
+            return $query->row_array();
+        }
+
+        public function get_forecast(){
+
+            $this->db->order_by('id','DESC');
+            $query = $this->db->get('forecasts');
+            return $query->result_array();
+        }
+
+        public function get_big_event($chpu = FALSE){
+
+
+            $query = $this->db->get_where('big_event', array('chpu' => $chpu));
+            return $query->row_array();
+        }
+}
